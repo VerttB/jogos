3a8e27f5ffe93f43c489b831f92d418c01ea4fc9
verttb <alyssonoliveira456@gmail.com>
2024-08-09 16:12:12 -0300

    novo componente e menu secundario

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import './Button.scss'
+type ButtonProps = {
+    onClick : () => void
+    children : String,
+}
+
+export default function Button({onClick, children} : ButtonProps){
+    return(
+        <button className='button' onClick={onClick}> 
+            {children}
+        </button>
+    )
+}
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,28 @@
+import Button from "../Button";
+import './Menu.scss';
+
+
+export const Menu = () => {
+
+    const jogar = () => {
+        console.log("Jogando");
+    }
+
+    const regras = () => {
+        console.log("regras");
+    }
+
+    const menuPrincipal = () => {
+        console.log("Menu Principal")
+    }
+    return(
+        <div className="menuSecundario">
+            <h3 className="titulo">Menu</h3>
+
+            <Button onClick={jogar}>Jogar</Button>
+            <Button onClick={regras}>Regras</Button>
+            <Button onClick={menuPrincipal}>Menu Principal</Button>
+        </div>
+    )
+
+}
