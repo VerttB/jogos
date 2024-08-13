dac7fa94e965204b154a510e1488a197b9bd1dc1
verttb <alyssonoliveira456@gmail.com>
2024-08-13 12:12:25 -0300

    mudança jogo da velha

--- src/components/JogoDaVelha/Jogo/index.tsx
+++ src/components/JogoDaVelha/Jogo/index.tsx
@@ -2,19 +2,34 @@ import { useState } from 'react';
 import Button from '../../Button';
 import TabuleiroJogoVelha from '../TabuleiroJogoVelha';
 import './JogoDaVelha.scss';
+import { MenuSecundario } from '../../MenuSecundario';
+
+interface BlocoArea{
+    conteudo : string,
+    jaClicado : boolean;
+    onClick: () => void
+}
 
 export const JogoDaVelha = () =>{
   
-    const [jogoIniciado, setJogoIniciado] = useState(false)
-
+    const [jogoIniciado, setJogoIniciado] = useState(false);
+    const [showMenu, setShowMenu] = useState(true);
+    const [jogadores, setJogadores] = useState(1)
     
     const iniciarJogo = () => {
         setJogoIniciado(true)
     }
 
+    const handleShowMenuChange = (status : boolean) : void => {
+        setShowMenu(status)
+    }
+
     return(
         <div className="jogoDaVelha">
-           <TabuleiroJogoVelha></TabuleiroJogoVelha>
+            {showMenu ?
+            <MenuSecundario fecharMenu={handleShowMenuChange} regras=''></MenuSecundario>
+            :
+           <TabuleiroJogoVelha></TabuleiroJogoVelha>}
         </div>
     )
 }
--- src/components/JogoDaVelha/TabuleiroJogoVelha/index.tsx
+++ src/components/JogoDaVelha/TabuleiroJogoVelha/index.tsx
@@ -5,14 +5,15 @@ import './TabuleiroJogoVelha.scss'
 export default function TabuleiroJogoVelha(){
 
    const jogar = (e : any ) => {
-     e.target.innerText = 'X'
+     e.target.style.backgroundImage = 'X'
+     console.log(e.target.style)
    }
 
 
 
     return(
         <div className='tabuleiroJogoVelha'>
-            {[...Array(9)].map((_, i) => <Button  key={i} onClick={jogar}>{''}</Button>)}
+            {[...Array(9)].map((_, i) => <div className="area" key={i} onClick={jogar}></div>)}
         </div>
     )
 }
