35d83ae2e6d596c0531aff07fc8fc8f5b22e80cc
verttb <alyssonoliveira456@gmail.com>
2024-08-14 10:43:35 -0300

    global scss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData: "@use \"src/assets/scss/global\" as g;"
+      }
+    }
+  }
 })
 
