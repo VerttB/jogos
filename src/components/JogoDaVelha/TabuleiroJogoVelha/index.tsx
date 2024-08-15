1901ad64a7469b1365b1648191adb31acddfae62
verttb <alyssonoliveira456@gmail.com>
2024-08-15 16:24:25 -0300

    jogo da velha, novo design

--- src/components/JogoDaVelha/TabuleiroJogoVelha/index.tsx
+++ src/components/JogoDaVelha/TabuleiroJogoVelha/index.tsx
@@ -1,17 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import  style  from './TabuleiroJogoVelha.module.scss'
-import { useState } from 'react';
+
 interface tabuleiroProps{
     
     areaClicada: string[]
     jogar : (id   : number) => void;
 }
 export default function TabuleiroJogoVelha({areaClicada, jogar } : tabuleiroProps){
-   
 
-    const divs = ([...Array(9)].map((_, i) => <div className={style.area} key={i} onClick={() => jogar(i)}>{areaClicada[i]}</div>))
-
+    const divs = ([...Array(9)].map((_, i) => <div className={style.area} key={i} onClick={() => jogar(i)}>{areaClicada[i]}</div>));
    
     return(
         <div className={style.tabuleiroJogoVelha}>
