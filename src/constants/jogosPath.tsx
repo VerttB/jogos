ae3b4fa04e4da0627bf38758d11d1f9f67cef4ef
verttb <alyssonoliveira456@gmail.com>
2024-08-16 23:14:52 -0300

    retirando coisas não utilizadas

--- src/constants/jogosPath.tsx
+++ src/constants/jogosPath.tsx
@@ -1,7 +1,6 @@
 import { JogoMemoria } from "../pages/JogoMemoria";
 import { JogoDaVelha } from "../pages/JogoDaVelha";
 import { JogoDaForca } from "../pages/JogoDaForca";
-import { JogoSudoku } from "../pages/JogoSudoku";
 
 interface CaminhoJogos {
     caminho : string,
@@ -14,6 +13,5 @@ export const jogosPath: Array<CaminhoJogos> = [
     {caminho : '/jogo-da-memoria', nome: 'Jogo Da Memoria', componente:<JogoMemoria/>},
     {caminho : '/jogo-da-velha', nome: 'Jogo Da Velha', componente:<JogoDaVelha/>},
     {caminho : '/jogo-da-forca', nome: 'Forca' , componente:<JogoDaForca/>},
-    {caminho : '/sudoku', nome:'Sudoku', componente:<JogoSudoku></JogoSudoku>}
   
 ]
