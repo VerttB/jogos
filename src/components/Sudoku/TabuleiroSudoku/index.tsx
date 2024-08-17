4ef04ea654222f38068d02466195b5b805a521b1
verttb <alyssonoliveira456@gmail.com>
2024-08-16 23:16:29 -0300

    .

--- src/components/Sudoku/TabuleiroSudoku/index.tsx
+++ src/components/Sudoku/TabuleiroSudoku/index.tsx
@@ -1,14 +1,12 @@
 import { BlocoPrimario } from '../BlocoPrimario'
-import { gerarMatriz } from '../Sudoku.config';
 import style from './TabuleiroSudoku.module.scss'
 
 export const TabuleiroSudoku = () =>{
     const blocosPrimarios = Array(9).fill('');
-    const matriz = gerarMatriz();
     return(
         <div className={style.tabuleiro}>
             {blocosPrimarios.map((_ ,i)=> <BlocoPrimario key={i}
-                                          valores={matriz.slice(i, (i + 9))}
+                                          valores={[]}
                                           ></BlocoPrimario>)}
         </div>
     )
