82dcacbf8964d82ddf210a8adbd6844abf04c796
verttb <alyssonoliveira456@gmail.com>
2024-08-18 22:30:18 -0300

    boneco jogo forca

--- src/components/Forca/Personagem/index.tsx
+++ src/components/Forca/Personagem/index.tsx
@@ -1,9 +1,50 @@
+import style from './Personagem.module.scss'
+
+interface sideProps{
+    side : 'right' | 'left'
+}
+const HEAD = () => {
+    return(
+        <div className={style.head}>
+           
+        </div>
+    )
+}
+
+const TORSO = () => {
+    return(
+        <div className={style.torso}>
+
+        </div>
+    )
+}
+
+const ARM = ({side} : sideProps) => {
+    return(
+        <div className={`${style.arm} ${style[side]}`}>
+
+        </div>
+    )
+}
+
+const LEG = ({side} :sideProps) => {
+    return(
+        <div className={`${style.leg} ${style[side]}`}>
+
+        </div>
+    )
+}
+
 export const Personagem = () => {
 
     return(
-
-        <div>Oi
-
-        </div>
+        <div className={style.personagem}>
+        <HEAD></HEAD>
+        <ARM side="right"></ARM>
+        <TORSO></TORSO>
+        <ARM side="left"></ARM>
+        <LEG side='right'></LEG>
+        <LEG side='left'></LEG>
+       </div>
     )
 }
