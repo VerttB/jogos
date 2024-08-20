71ebdce87b613b024907eac7fb0392a8039f3c3a
verttb <alyssonoliveira456@gmail.com>
2024-08-19 22:44:05 -0300

    funcoes jogo forca

--- src/pages/JogoDaForca/index.tsx
+++ src/pages/JogoDaForca/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { Palavra } from "../../components/Forca/Palavra"
 import { Personagem } from "../../components/Forca/Personagem"
 import style from './JogoDaForca.module.scss';
 
 export const JogoDaForca = () => {
+    const [secretWord, setSecretWord] = useState<string[]>("Carro".split(''));
+    const [guessedCharacter, setGuessedCharacter] = useState<string[]>();
+    const [guessedWord, setGuessedWord] = useState([])
+    const [tries, setTries] = useState(5);
 
+    useEffect( () => {
+        if(tries === 0)
+    },[tries])
+
+    useEffect(() => {
+        if(guessedWord === secretWord)
+    },[guessedWord])
     return(
         <div className={style.jogoDaForca}>
         <Personagem></Personagem>
